c2d3dd98ab6e871b475c3a82deb15df2555e34cb
whitzhu <whitzhu@gmail.com>
2017-02-06 18:31:29 -0800

    getElementsbyClass complete

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -20,7 +20,9 @@ describe('getElementsByClassName', function() {
       var expectedNodeList = document.getElementsByClassName('targetClassName');
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
-      expect(equality).to.equal(FILL_ME_IN);
+      
+      console.log( expectedArray);
+      expect(equality).to.equal(true);
 
       $rootElement.remove();
     });
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element, result) {
+
+  if (element === undefined) {
+    element = document.body;
+    result = [];
+  }
+  
+  if (element.className !== undefined && element.className.length > 0) {
+    if (element.className.indexOf(className) !== -1 ){
+      result.push(element);  
+    }
+  }
+
+  if ( element.childNodes.length > 0) {
+    for (var i = 0; i < element.childNodes.length; i++) {
+      getElementsByClassName(className, element.childNodes[i], result);
+    }
+  }
+
+  return result; 
+  
 };
